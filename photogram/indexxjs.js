ff3b2703e581d8728f14b7347eb91051511dfa60
Soumya Deb <soumyadeb1809@gmail.com>
2018-02-24 22:44:40 +0530

    initial commit

---
+++ photogram/indexxjs.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.display = "inline";
+
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
